13555f6002e147e53d4d45fabb349a3f39c7845d
Nguyen Tan Bao <baont@sendo.vn>
2020-04-24 11:16:06 +0700

    make get heroes function

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 handlers.getHeroes = function (args, context) {
-
+    playerTag = currentPlayerId;
+    if (args && args.playerTag) {
+        playerTag
+    }
+    
     var playerData = server.GetUserInternalData({
         PlayFabId: currentPlayerId,
         Keys: ["heroes"]
     });
 
-    return playerData;
+    const heroes = playerData.Data["heroes"];
+
+    return {
+        userHeroes: heroes,
+        playerTag
+    };
 };
